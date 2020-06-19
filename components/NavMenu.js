975cf7d912809ffba178ff525eb66d54c9c68505
Webrobika1 <webrobika@gmail.com>
2020-06-20 01:53:01 +0200

    1 file added, 1 deleted

---
+++ components/NavMenu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavMenu extends Component {
+  render(){
+    return (
+      <nav>
+        <a href="">Home</a>
+        <a href="">Pricing</a>
+        <a href="">About us</a>
+      </nav>
+    );
+  }
+}
